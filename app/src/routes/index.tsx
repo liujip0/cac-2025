ed2b0f479f19e35bb9347784372068e28bd6827e
liujip0 <liujip2020@gmail.com>
2025-07-21 15:13:27 -0400

    add top bar to app root

--- app/src/routes/index.tsx
+++ app/src/routes/index.tsx
@@ -1,5 +1,10 @@
+import TopBar from "src/components/TopBar/TopBar.tsx";
 import globalStyles from "../globalStyles.module.css";
 
 export default function Index() {
-  return <div className={globalStyles.screenContainer}></div>;
+  return (
+    <div className={globalStyles.screenContainer}>
+      <TopBar />
+    </div>
+  );
 }
