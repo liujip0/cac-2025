2a5f79ec1f56dc0e4522efc54d3a91ba5cc9c971
liujip0 <liujip2020@gmail.com>
2025-08-07 11:36:31 -0400

    some progress on infochips

--- app/src/components/Internship/Internship.tsx
+++ app/src/components/Internship/Internship.tsx
@@ -1,3 +1,4 @@
+import type React from "react";
 import styles from "./Internship.module.css";
 
 export default function Internship() {
@@ -5,17 +6,53 @@ export default function Internship() {
     <div className={styles.internship}>
       <h2 className={styles.title}>Some Internship</h2>
       <div className={styles.quickInfo}>
-        <InfoChip />
+        <InfoChip
+          icon={
+            <span className="material-symbols-outlined">calendar_month</span>
+          }>
+          May 2024 - Aug 2024
+        </InfoChip>
+        <InfoChip
+          icon={<span className="material-symbols-outlined">location_on</span>}>
+          Remote
+        </InfoChip>
+        <InfoChip
+          icon={<span className="material-symbols-outlined">schedule</span>}>
+          Full-time
+        </InfoChip>
+        <InfoChip
+          icon={
+            <span className="material-symbols-outlined">attach_money</span>
+          }>
+          $10/hr
+        </InfoChip>
+        <InfoChip
+          icon={<span className="material-symbols-outlined">school</span>}>
+          High School
+        </InfoChip>
       </div>
+      <p className={styles.description}>
+        Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus
+        ex sapien vitae pellentesque sem placerat. In id cursus mi pretium
+        tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.
+        Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis
+        massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper
+        vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra
+        inceptos himenaeos.
+      </p>
     </div>
   );
 }
 
-function InfoChip() {
+type InfoChipProps = {
+  icon: React.ReactNode;
+  children?: React.ReactNode;
+};
+function InfoChip({ icon, children }: InfoChipProps) {
   return (
     <div className={styles.infoChip}>
-      <span className="material-symbols-outlined">attach_money</span>
-      $10/hr
+      {icon}
+      {children}
     </div>
   );
 }
