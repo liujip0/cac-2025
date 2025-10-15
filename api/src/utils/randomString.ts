4de3572198788bd3290fe23c8473e74980feda13
liujip0 <liujip2020@gmail.com>
2025-10-15 14:58:54 -0400

    add dashes and underscores to randomString

--- api/src/utils/randomString.ts
+++ api/src/utils/randomString.ts
@@ -1,6 +1,6 @@
 export function randomString(length: number) {
   const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
   let result = "";
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters.length));
